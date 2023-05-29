ce9f4bee83b4c5bd5bbc1ba26d5239e93e714f62
Muhammed R <64767974+muhammedr7025@users.noreply.github.com>
2023-05-29 05:31:56 +0530

    Update numberguess.py

--- numberguess.py
+++ numberguess.py
@@ -2,10 +2,10 @@ import random
 print("GUESS THE NUMBER \n")
 rand=random.randrange(1,50)
 a=0
-c=1
-while a<=c:
+c=3
+while a<c:
     num=int(input("Guess a number between 1&50: "))
-    c=c+1
+    a=c+1
     if rand==num:
         print("you have won!")
     else:
